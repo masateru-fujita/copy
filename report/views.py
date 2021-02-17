7f82d7a3ff776c7284affbd10642a85ccd4e93c3
unknown <cl.m.fujita@gmail.com>
2021-02-17 18:58:02 +0900

    Chart.js導入、アクセス/アクション回数完成

--- report/views.py
+++ report/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 from project.models import Project
 from video.models import VideoRelation, Video, LinkTag, EndTag
 from view.models import UserAnalysis, ActionAnalysis
 from project.serializers import *
 from video.serializers import *
+from view.serializers import *
 
 import json
 
 def report(request):
     return render(request, 'report/report.html')
 
-class ProjectAPIView(ListCreateAPIView):
+class ProjectAPIView(ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+class AnalysisAPIView(ListAPIView):
+    queryset = UserAnalysis.objects.all()
+    serializer_class = UserAnalysisSerializer
---
+++ view/serializers.py
@@ -0,0 +1,56 @@
+from .models import UserAnalysis, ActionAnalysis
+from video.models import LinkTag
+from rest_framework.serializers import Field, SerializerMethodField
+from rest_framework import serializers
+
+import datetime
+
+class UserAnalysisSerializer(serializers.ModelSerializer):
+    actionAnalysis = SerializerMethodField()
+    class Meta:
+        model = UserAnalysis
+        fields = [
+            'id',
+            'user_agent',
+            'video_relation',
+            'user_cookie',
+            'access_time',
+            'leave_time',
+            'start_time',
+            'end_time',
+            'actionAnalysis',
+        ]
+
+    def get_actionAnalysis(self, obj):
+        # try:
+            action_analysis_abstruct_contents = ActionAnalysisSerializer(ActionAnalysis.objects.all().filter(user_analysis = UserAnalysis.objects.get(id=obj.id)), many=True).data
+            return action_analysis_abstruct_contents
+        # except:
+        #     action_analysis_abstruct_contents = None
+        #     return action_analysis_abstruct_contents
+
+class ActionAnalysisSerializer(serializers.ModelSerializer):
+    story_play_time = SerializerMethodField()
+    class Meta:
+        model = ActionAnalysis
+        fields = [
+            'id',
+            'user_analysis',
+            'tag',
+            'action_type',
+            'action_time',
+            'switch_video',
+            'story_end_time',
+            'popup_btn_flg',
+            'story_play_time',
+        ]
+
+    def get_story_play_time(self, obj):
+        try:
+            story_start_flame = LinkTag.objects.get(pk=obj.tag.pk).story_start_flame
+            start_time = int(story_start_flame) / 30
+            story_play_time_abstruct_contents = datetime.datetime.combine(datetime.date.today(), obj.story_end_time) - datetime.timedelta(seconds=start_time)
+            return story_play_time_abstruct_contents.strftime("%H:%M:%S.%f")
+        except:
+            story_play_time_abstruct_contents = None
+            return story_play_time_abstruct_contents
